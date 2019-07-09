a4fa8f2fb4c4a1aa86840805dfaea460798775b0
Szymon Matuszewski <szymon.matuszewski@pronos.eu>
2019-07-09 15:48:47 +0200

    Add primary connection string to output

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,11 @@ output "name" {
   description = "The name of the storage account."
 }
 
+output "primary_connection_string" {
+  value       = azurerm_storage_account.main.primary_connection_string
+  description = "The primary connection string for the storage account."
+}
+
 output "primary_access_key" {
   value       = azurerm_storage_account.main.primary_access_key
   description = "The primary access key for the storage account."
